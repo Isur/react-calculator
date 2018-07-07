46a78720a849799174eff52b66bcd1b7c17e8c4d
Isur <bednarczykartur96@gmail.com>
2018-07-08 00:49:58 +0200

    working calculator

--- src/App.js
+++ src/App.js
@@ -1,113 +1,149 @@
 import React from 'react';
 import './App.css';
-import _ from 'lodash';
 
-
-const Equal = (props) =>{
-    return(<div>
-      <button onClick={props.equal}> = </button>
-      <p> {props.result}</p></div>
-    );
+const Button = (props) => {
+  return(
+    <button onClick={props.function} className={props.cl} id={props.id}>{props.value}</button>
+  );
 }
 
-
-const Operators = (props) =>{
+const Buttons = (props) => {
   return(
-    <div>
-      <button onClick={() => props.setOperator("+")}> + </button>
-      <button onClick={() => props.setOperator("-")}> - </button>
-      <button onClick={() => props.setOperator("/")}> / </button>
-      <button onClick={() => props.setOperator("*")}> * </button>
+    <div className="Buttons">
+      <Button cl="ButtonNumbers" value="7" id="button_7" function={() => props.number(7)} />
+      <Button cl="ButtonNumbers" value="8" id="button_8" function={() => props.number(8)} />
+      <Button cl="ButtonNumbers" value="9" id="button_9" function={() => props.number(9)} />
+      <Button cl="ButtonNumbers" value="+" id="button_+" function={() => props.operator("+")}/>
+      <Button cl="ButtonNumbers" value="4" id="button_4" function={() => props.number(4)} />
+      <Button cl="ButtonNumbers" value="5" id="button_5" function={() => props.number(5)} />
+      <Button cl="ButtonNumbers" value="6" id="button_6" function={() => props.number(6)} />
+      <Button cl="ButtonNumbers" value="-" id="button_-" function={() => props.operator("-")}/>
+      <Button cl="ButtonNumbers" value="1" id="button_1" function={() => props.number(1)} />
+      <Button cl="ButtonNumbers" value="2" id="button_2" function={() => props.number(2)} />
+      <Button cl="ButtonNumbers" value="3" id="button_3" function={() => props.number(3)} />
+      <Button cl="ButtonNumbers" value="*" id="button_*" function={() => props.operator("*")}/>
+      <Button cl="ButtonNumbers" value="C" id="button_C" function={props.clear}/>
+      <Button cl="ButtonNumbers" value="0" id="button_0" function={() => props.number(0)} />
+      <Button cl="ButtonNumbers" value="=" id="button_=" function={props.equal}/>
+      <Button cl="ButtonNumbers" value="/" id="button_/" function={() => props.operator("/")}/>
     </div>
   );
 }
 
-const Result = (props) =>{
+const Text = (props) => {
   return(
-    <h3> {props.firstNumber} {props.operator} {props.secondNumber} = {props.result} </h3>
-  );
-}
-
-const Form = () =>{
-  return(
-    <form>
-      <input placeholder="Input your number" />
-    </form>
-  );
-}
-
-const Numbers = (props) =>{
-  return(
-    <div className="Numbers"> 
-    {_.range(1,10).map(i =>
-       <button key={i} onClick={() => props.setNumber(i)}>{i}</button>
-      )}
+    <div className="Result">
+      <p className="ResultText">{props.firstNumber} {props.operator} {props.secondNumber} {props.resultNumber && `=`} {props.resultNumber}</p>
     </div>
   )
 }
 
-class App extends React.Component {
-  state = {
-    a: null,
-    b: null,
-    result: null,
+class App extends React.Component{
+constructor(props){
+  super(props);
+  this.state ={
+    firstNumber: null,
+    secondNumber: null,
     operator: null,
+    resultNumber: null
   }
+}
 
-  setNumber = (number) =>{
-    this.setState(prevState => {
-      if(prevState.a === null){
-        return {a: number};
-      }else if(prevState.operator !== null){
-        return {b: number};
+  number = (num) => {
+    if(this.state.firstNumber === null){
+      this.setState(() => {
+       if(num !== 0) return({ firstNumber: num })
+      });
+    }else if(this.state.operator === null){
+      this.setState(prevState => ({
+        firstNumber: `${prevState.firstNumber}${num}`
+      }));
+    }else if(this.state.operator !== null && this.state.resultNumber === null){
+      if(this.state.secondNumber === null){
+        this.setState(() => {
+          if(num !== 0) return({ secondNumber: num })
+         });
+      }else{
+        this.setState(prevState => ({
+          secondNumber: `${prevState.secondNumber}${num}`
+        }));
       }
-    });
+    }else if(this.state.resultNumber !== null){
+      if(num === 0) return;
+      this.setState({
+        firstNumber: num,
+        secondNumber: null,
+        operator: null,
+        resultNumber: null,
+      })
+    }
+    
   }
+  operator = (op) => {
+    if(this.state.firstNumber !== null && this.state.secondNumber === null){
+      this.setState({
+        operator: op
+      })
+    }else if(this.state.resultNumber !== null){
+      this.setState(prevState => ({
+        firstNumber: prevState.resultNumber,
+        secondNumber: null,
+        operator: op,
+        resultNumber: null
+      }));
+    }else if(this.state.firstNumber !== null && this.state.secondNumber !== null && this.state.resultNumber === null){
+      this.setState({
+        operator: op
+      });
+    }
 
-  setOperator = (operator) =>{
-    this.setState(prevState => {
-      if(prevState.a === null){
-        return;
-      }
-      return {operator: operator};
-    });
   }
-  calculate = () =>{
-    this.setState(prevState => {
-      if(prevState.operator !== null && prevState.a !== null && prevState.b !== null){
-        if(prevState.operator === "+"){
-          return{result: prevState.a + prevState.b}
+  equal = () => {
+    if(this.state.firstNumber !== null && this.state.secondNumber !== null && this.state.operator !== null){
+      this.setState(prevState => {
+        switch(prevState.operator){
+          case "+":
+            return ({resultNumber: prevState.firstNumber + prevState.secondNumber});
+          break;
+          case "-":
+          return ({resultNumber: prevState.firstNumber - prevState.secondNumber});
+          break;
+          case "*":
+          return ({resultNumber: prevState.firstNumber * prevState.secondNumber});
+          break;
+          case "/":
+          return ({resultNumber: prevState.firstNumber / prevState.secondNumber});
+          break;
         }
-        if(prevState.operator === "-"){
-          return{result: prevState.a - prevState.b}
-        }
-        if(prevState.operator === "*"){
-          return{result: prevState.a * prevState.b}
-        }
-        if(prevState.operator === "/"){
-          return{result: prevState.a / prevState.b}
-        }
-      }
-    });
+      });
+    }
   }
-  render() {
-    return (
-      <div className="App">
-        <Numbers 
-            setNumber={this.setNumber} />
-        <Form />
-        <Operators setOperator={this.setOperator} />
-        <Result 
-            firstNumber={this.state.a} 
-            secondNumber={this.state.b} 
-            operator={this.state.operator} 
-            result={this.state.result} />
-            <Equal 
-                result={this.state.result}
-                equal={this.calculate} />
-      </div>
+  clear = () => {
+    this.setState({
+      firstNumber: null,
+      secondNumber: null,
+      operator: null,
+      resultNumber: null
+    })
+  }
+  render(){
+    return(
+        <div className="Calculator">
+          <Text 
+          firstNumber={this.state.firstNumber}
+          secondNumber={this.state.secondNumber}
+          resultNumber={this.state.resultNumber}
+          operator={this.state.operator}
+          />
+          <Buttons 
+          number={this.number} 
+          operator={this.operator}
+          equal={this.equal}
+          clear={this.clear}
+          />
+        </div>
     );
-  }
+  };
 }
 
 export default App;
